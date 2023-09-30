93fca02d5dd1b7633de110af8232a1f379102e05
kgenots <kgenots@gmail.com>
2023-09-30 19:55:06 +0900

    Updates

--- Assets/HexaBlast/Scripts/StageScene/Tools/CameraSetter.cs
+++ Assets/HexaBlast/Scripts/StageScene/Tools/CameraSetter.cs
@@ -4,8 +4,8 @@ namespace HexaBlast
 {
    class CameraSetter : MonoBehaviour
    {
-      public float rowPadding = 0.5f;
-      public float colPadding = 0.1f;
+      public float rowPadding = 1f;
+      public float colPadding = 1f;
       public bool m_updateCameraWithValidation;
 
       private void OnValidate()
@@ -27,8 +27,8 @@ namespace HexaBlast
 
          var rowSize = 0f;
          var colSize = 0f;
-         rowSize = map.RowSize * map.RowGap * (1 + rowPadding) * cam.aspect;
-         colSize = map.ColSize * map.ColGap * (1 + colPadding);
+         rowSize = map.RowSize * map.RowGap * (0.7f + rowPadding) * cam.aspect;
+         colSize = map.ColSize * map.ColGap * (0.7f + colPadding);
          cam.orthographicSize = Mathf.Max(rowSize, colSize);
       }
    }
